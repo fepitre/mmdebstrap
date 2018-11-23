96c01a67e4834b1fff14bfc67f05dfd223d2f7c9
Johannes 'josch' Schauer <josch@mister-muffin.de>
2018-11-23 17:36:20 +0100

    make_mirror.sh: use redirection to write tarfile
    During the Debian autopkgtest mmdebstrap is executed as root but we
    don't want the tarfile being owned by root.

--- make_mirror.sh
+++ make_mirror.sh
@@ -160,9 +160,12 @@ mkdir -p "$cachedir"
 # We do not use our own package cache here because
 #   - it doesn't (and shouldn't) contain the extra packages
 #   - it doesn't matter if the base system is from a different mirror timestamp
+# Write tarfile via redirection because in the Debian package autopkgtest
+# ./mmdebstrap is a suid binary and we do not want the tarfile being owned
+# by root.
 ./mmdebstrap --variant=apt --architectures=amd64,armhf --mode=unshare \
 	--include=linux-image-amd64,systemd-sysv,perl,arch-test,fakechroot,fakeroot,mount,uidmap,proot,qemu-user-static,binfmt-support,qemu-user,dpkg-dev,mini-httpd,libdevel-cover-perl,debootstrap,libfakechroot:armhf,libfakeroot:armhf \
-	unstable debian-unstable.tar
+	unstable > debian-unstable.tar
 
 cat << END > extlinux.conf
 default linux
