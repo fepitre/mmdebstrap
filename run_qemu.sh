7815fd16e6615546c0e6cde46eeb943edab82a31
Johannes 'josch' Schauer <josch@mister-muffin.de>
2020-01-03 15:52:25 +0100

    run_qemu.sh: run qemu with -no-user-config

--- run_qemu.sh
+++ run_qemu.sh
@@ -29,7 +29,9 @@ trap cleanup INT TERM EXIT
 qemu-img create -f qcow2 -b "$(realpath $cachedir)/debian-$DEFAULT_DIST.qcow" "$tmpdir/debian-$DEFAULT_DIST-overlay.qcow"
 # to connect to serial use:
 #   minicom -D 'unix#/tmp/ttyS0'
-qemu-system-x86_64 -M accel=kvm:tcg -m 1G -nographic \
+qemu-system-x86_64 \
+	-no-user-config \
+	-M accel=kvm:tcg -m 1G -nographic \
 	-monitor unix:/tmp/monitor,server,nowait \
 	-serial unix:/tmp/ttyS0,server,nowait \
 	-serial unix:/tmp/ttyS1,server,nowait \
